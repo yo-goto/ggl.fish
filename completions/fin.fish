6a4677cdc7bf45c5562e69cdcf154da7861be1b2
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-06-12 10:21:50 +0900

    fixed a spell mistake

--- completions/fin.fish
+++ completions/fin.fish
@@ -7,8 +7,8 @@ complete -c fin -l long -f -d "Show the long list of all urls & sites"
 complete fin -x -n __fish_use_subcommand -a ggl -d "Use ggl"
 complete fin -x -n __fish_use_subcommand -a help -d "Show help"
 complete fin -x -n __fish_use_subcommand -a ls -d "Show list"
-## query exits
-complete fin -x -n __fish_use_subcommand -a 'mdn codepen angular npm emojipedia rust tokio vscode neovim'
+## query exists
+complete fin -x -n __fish_use_subcommand -a 'mdn codepen angular npm npmtrends emojipedia rust tokio vscode neovim'
 ## query doesn't exist
 complete fin -x -n __fish_use_subcommand -a 'node deno vue react typescript storybook bem nextjs yarn tmux iterm2 svelte tailwindcss gh git daisyui chakraui mui'
 ## from ggl options
